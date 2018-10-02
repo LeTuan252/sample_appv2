0322b044a8dfe446daec94a46e1691a327719a54
LeTuan252 <43332285+LeTuan252@users.noreply.github.com>
2018-10-02 08:28:23 +0700

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -1,9 +1,9 @@
 require_relative 'boot'
 require 'rails/all'
-Bundler.require(*Rails.groups)
-  module SampleAppv2
-    class Application < Rails::Application
-      config.load_defaults 5.2
-      config.i18n.default_locale = :en
-    end
-end
+  Bundler.require(*Rails.groups)
+    module SampleAppv2
+      class Application < Rails::Application
+        config.load_defaults 5.2
+        config.i18n.default_locale = :en
+      end
+  end
